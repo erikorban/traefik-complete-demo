0fa6f86ff0faa6989ff8fc05e05d678b6aa2177b
Erik Orban <erikorban0@gmail.com>
2021-11-05 17:18:17 +0200

    Create base projects

---
+++ webapi/Controllers/ServiceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServiceController : ControllerBase
+    {
+        public ServiceController()
+        { }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(Environment.GetEnvironmentVariable("MESSAGE"));
+        }
+    }
+}
